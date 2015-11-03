c09f8f1554ddece46e7929faba62f17504a161d6
Yabu Kiyohide <takustaqu@gmail.com>
2015-11-04 01:04:39 +0900

    sleep to setTimeout

--- edison/app.js
+++ edison/app.js
@@ -4,7 +4,11 @@ var servo = new mraa.Pwm(3);//Pin 3
 servo.enable(true);
 servo.period_us(19000000) //PWM Period https://www.arduino.cc/en/Tutorial/SecretsOfArduinoPWM
 servo.write(1);
-sleep(1000);
-servo.write(0);
-sleep(1000);
-servo.write(0.5);
+
+setTimeout(function(){
+	servo.write(0);
+},1000)
+
+setTimeout(function(){
+	servo.write(0.5);
+},2000)
