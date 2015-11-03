51951e40c28d634051027177dd161405e4211fe0
Yabu Kiyohide <takustaqu@gmail.com>
2015-11-04 01:34:53 +0900

    LEDテスト

--- edison/app.js
+++ edison/app.js
@@ -1,14 +1,24 @@
 var mraa = require("mraa");
+var io = require('socket.io-client');
 var servo = new mraa.Pwm(3);//Pin 3
+var led = new mraa.Gpio(8);
+led.dir(mraa.DIR_OUT);
 
 servo.enable(true);
 servo.period_us(19000000) //PWM Period https://www.arduino.cc/en/Tutorial/SecretsOfArduinoPWM
 servo.write(1);
 
+var socket = io('http://localhost:3000');
+  socket.on('connect', function () {
+    socket.emit("message", 'send message.');
+});
+
 setTimeout(function(){
 	servo.write(0);
+	led.write(1);
 },1000)
 
 setTimeout(function(){
-	servo.write(1);
+	servo.write(0.5);
+	led.write(0);
 },2000)
