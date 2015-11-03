12b3eaab27480e61dd45b7d70fa9b73792c658d1
Yabu Kiyohide <takustaqu@gmail.com>
2015-11-04 01:02:01 +0900

    servo test

--- edison/app.js
+++ edison/app.js
@@ -1,1 +1,10 @@
 var mraa = require("mraa");
+var servo = new m.Pwm(3);//Pin 3
+
+servo.enable(true);
+servo.period_us(19000000) //PWM Period https://www.arduino.cc/en/Tutorial/SecretsOfArduinoPWM
+servo.write(1);
+sleep(1000);
+servo.write(0);
+sleep(1000);
+servo.write(0.5);
